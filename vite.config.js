a0b7f4260cbcaa6bae53b21e2de31d05fd8d9c74
Philipp Röhm <proehm@stud.hs-heilbronn.de>
2025-06-20 12:26:00 +0200

    Fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://pillepalle1893.github.io/skydiving-cat-parallax',
   plugins: [react()]
 })
